80f7207eecdb83929ad4e2b46d9b6261882e5ba0
joshchunghung <joshchunghung@gmail.com>
2022-03-05 16:14:14 +0800

    setup細節

---
+++ vue3.study/05_setUp/App.vue
@@ -0,0 +1,30 @@
+<template>
+<h2>app父級組件</h2>
+<h4>msg:{{msg}}</h4>
+<button @click="msg += '++'" >更新數據</button>
+<br>
+<childT :msg="msg"  msg2="good" @xxx="xxx"></childT>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+// 引入子級組件
+import childT from "./component/child.vue"
+export default defineComponent({
+  name: 'App',
+  // 註冊組件
+  components:{
+    childT
+  },
+  setup() {
+    const msg = ref('hello')
+    function xxx (txt : string) {
+      msg.value += txt
+    }
+    return{
+      msg,
+      xxx
+    }
+  }
+})
+</script>
