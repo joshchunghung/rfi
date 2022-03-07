41b416ea3d2c56972fdbc45ac6639a9dce954989
joshchunghung <joshchunghung@gmail.com>
2022-03-07 12:18:22 +0800

    生命週期

---
+++ vue3.study/09_lifecycle/App.vue
@@ -0,0 +1,23 @@
+<template>
+<h2>父級組件</h2>
+<button @click="isShow=!isShow">切換顯示</button>
+<hr>
+<childT v-if="isShow"></childT>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import childT from './components/child.vue'
+export default defineComponent({
+  name: 'App',
+  components:{
+    childT
+  },
+  setup(){
+    const isShow = ref(true)
+    return {
+      isShow
+    }
+  }
+})
+</script>
---
+++ vue3.study/09_lifecycle/components/child.vue
@@ -0,0 +1,74 @@
+<template>
+<h2>子級組件</h2>
+<h3>msg:{{msg}}</h3>
+<button @click="update">更新</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref ,  
+onMounted,
+onUpdated,
+onUnmounted, 
+onBeforeMount, 
+onBeforeUpdate,
+onBeforeUnmount} from 'vue'
+
+export default defineComponent({
+    name: 'childT',
+    // vue2的生命週期
+    beforeCreate(){
+        console.log("vue2..beforeCreate")
+    },
+    created(){
+        console.log("vue2..created")
+    },
+    beforeMount(){
+    console.log("vue2..beforeMount")
+    },
+    mounted(){
+        console.log("vue2..mounted")
+    },
+    beforeUpdate(){
+        console.log("vue2..beforeUpdate")
+    },
+    updated(){
+        console.log("vue2..updated")
+    },
+    beforeUnmount() {  
+        console.log("vue2..beforeDestroy")
+    },
+    unmounted(){
+        console.log("vue2..destroyed")
+    },
+    setup(){
+        console.log("vue3.. setup")
+        const msg = ref("abc")
+        const update = () =>{
+            msg.value += "---"
+        }
+
+    onMounted(()=>{
+        console.log("vue3 onMounted")
+    })
+    onUpdated(()=>{
+        console.log("vue3 onUpdated")
+    })
+    onUnmounted(()=>{
+        console.log("vue3 onUnmounted")
+    })
+    onBeforeMount(()=>{
+        console.log("vue3 onBeforeMount")
+    })
+    onBeforeUpdate(()=>{
+        console.log("vue3 onBeforeUpdate")
+    })
+    onBeforeUnmount(()=>{
+        console.log("vue3 onBeforeUnmount")
+    })
+    return{ 
+        msg,
+        update
+        }
+    }
+})
+</script>
